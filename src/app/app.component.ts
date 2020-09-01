3e2d764fba7743a03c35dba722f18fa470ead104
Sven Moon <svenpire@gmail.com>
2020-08-31 20:44:13 -0400

    select for both model & type; console.log doesn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,21 @@ export class AppComponent {
     {"id":2,"name": "Android"},
     {"id":3,"name": "Other"}
    ];
-  selectedPhoneModelId = -1;
+
+  phoneTypes = [
+    {"id":-1,"name": "Select Phone Model--"},
+    {"id":1,"name": "iPhone 7"},
+    {"id":2,"name": "iPhone 8"},
+    {"id":3,"name": "iPhone 9"},
+    {"id":4,"name": "iPhone X"},
+    {"id":5,"name": "Pixel 1"},
+    {"id":6,"name": "Pixel 2"},
+  ]
 
   public  onSelectedPhoneModelIdChange(id) {
+    console.log(id)
+  }
+  public onSelectedPhoneModelIdSelect(id) {
     console.log(id)
   }
 }
