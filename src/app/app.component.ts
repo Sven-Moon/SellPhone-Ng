45b0e309254f48ca8a3f7793123c58ddbbf9cdc0
Sven Moon <svenpire@gmail.com>
2020-08-31 20:12:25 -0400

    can't get select to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NGB_DATEPICKER_18N_FACTORY } from '@ng-bootstrap/ng-bootstrap/datepicker/datepicker-i18n';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sellphone-ng';
+  phoneModels = [
+    {"id":-1,"name": "--Select Phone Model--"},
+    {"id":1,"name": "iPhone"},
+    {"id":2,"name": "Android"},
+    {"id":3,"name": "Other"}
+   ];
+  selectedPhoneModelId = -1;
+
+  public  onSelectedPhoneModelIdChange(id) {
+    console.log(id)
+  }
 }
