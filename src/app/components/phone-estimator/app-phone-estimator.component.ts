b134567018eb4d5af6bc48c9311c516aac1b1847
Sven Moon <svenpire@gmail.com>
2020-10-05 21:37:19 -0400

    dropdowns working

--- src/app/components/phone-estimator/app-phone-estimator.component.ts
+++ src/app/components/phone-estimator/app-phone-estimator.component.ts
@@ -17,19 +17,42 @@ export class AppPhoneEstimatorComponent implements OnInit {
   selectedType: String = "--Choose Type--";
   selectedModel: String = "--Choose Model--";
 
-  phones = [
-    {type: "iPhone", models: [ "iPhone 7", "iPhone 8", "iPhone 9", "iPhone X"]},
-    {type: "Android", models: [ "Pixel 2", "Pixel 3", "Pixel 3a", "Pixel 4"]},
-    {type: "Other", models: [ "Windows", "UPort", "Nokia"]}
+  phoneTypes = [
+    {"id": -1, "name": "-- Select Phone type -- "},
+    {"id": 1, "name": "iPhone "},
+    {"id": 2, "name": "Android "},
+    {"id": 3, "name": "Other"},
    ];
+   phoneModels = [];
+   phoneValue: String = "";
+   isPhoneValuePanelVisible: boolean = false;
 
-   models: Array<any>;
-
-  public  changeType(type) {    
-    this.models = this.phones.find(list => list.type == type).models
-    this.selectedModel = "--Choose Model--"
+  public onSelectedPhoneTypeChange(e) {
+    console.log(e);
+    let typeId = e.target.selectedOptions[0].id;
+    console.log("newly selected phoneType is: " + typeId);    
+    this.phoneModels = this.getPhoneModelsByType(typeId);
   }
-  public selectModel(id) {
-    console.log(id)
+
+  public onSelectedPhoneModelChange(e) {
+    let modelId = e.target.selectedOptions[0].id;
+    console.log("new phone Model ID is: " + modelId);
+  }
+
+  private getPhoneModelsByType(typeId) {
+    console.log(typeId);
+    if (typeId == 1) {
+      return [
+        { "id": 1, "name": "iPhone 7" },
+        { "id": 2, "name": "iPhone 8" }
+      ]
+    } else if (typeId == 2) {
+      return [
+        { "id": 5, "name": "Pixel 1" },
+        { "id": 5, "name": "Pixel 2" },
+      ]
+    } else {
+      return [];
+    }
   }
 }
