2858c7cdcaa1c64cfe0ce4128977ba51193c8bfa
Sven Moon <svenpire@gmail.com>
2020-10-07 21:18:27 -0400

    phone value estimate "working"

---
+++ src/app/components/app/models/PhoneType.ts
@@ -0,0 +1,4 @@
+export class PhoneType {
+  id: number; 
+  name: String; 
+}
--- src/app/components/phone-estimator/app-phone-estimator.component.ts
+++ src/app/components/phone-estimator/app-phone-estimator.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { PhoneType } from '../app/models/PhoneType';
+import { PhoneModel } from '../app/models/PhoneModel';
+
 
 @Component({
   selector: 'app-phone-estimator',
@@ -17,42 +20,54 @@ export class AppPhoneEstimatorComponent implements OnInit {
   selectedType: String = "--Choose Type--";
   selectedModel: String = "--Choose Model--";
 
-  phoneTypes = [
+  phoneTypes:Array<PhoneType> = [
     {"id": -1, "name": "-- Select Phone type -- "},
     {"id": 1, "name": "iPhone "},
     {"id": 2, "name": "Android "},
     {"id": 3, "name": "Other"},
    ];
-   phoneModels = [];
-   phoneValue: String = "";
-   isPhoneValuePanelVisible: boolean = false;
+   phoneModels:Array<PhoneModel> = [];
+   phoneMaxValue: String = "";
+   isValueBoxVisible: boolean = false;
 
-  public onSelectedPhoneTypeChange(e) {
+  public onSelectedPhoneTypeChange(e:any):void {
     console.log(e);
     let typeId = e.target.selectedOptions[0].id;
     console.log("newly selected phoneType is: " + typeId);    
     this.phoneModels = this.getPhoneModelsByType(typeId);
   }
 
-  public onSelectedPhoneModelChange(e) {
+  public onSelectedPhoneModelChange(e):void {
     let modelId = e.target.selectedOptions[0].id;
     console.log("new phone Model ID is: " + modelId);
+    this.phoneMaxValue = this.onPhoneModelSelect(modelId);
   }
 
-  private getPhoneModelsByType(typeId) {
+  public getPhoneModelsByType(typeId:number):Array<PhoneModel> {
     console.log(typeId);
     if (typeId == 1) {
       return [
         { "id": 1, "name": "iPhone 7" },
-        { "id": 2, "name": "iPhone 8" }
+        { "id": 2, "name": "iPhone 8" },
+        { "id": 3, "name": "iPhone 9" },
+        { "id": 4, "name": "iPhone X" }
       ]
     } else if (typeId == 2) {
       return [
         { "id": 5, "name": "Pixel 1" },
-        { "id": 5, "name": "Pixel 2" },
+        { "id": 6, "name": "Pixel 2" },
+      ]
+    } else if (typeId == 3) {
+      return [
+        { "id": 5, "name": "Windows" },
+        { "id": 6, "name": "Uport" },
       ]
     } else {
       return [];
     }
+  }
+  private onPhoneModelSelect(id:number):string{
+    this.isValueBoxVisible = true;
+    return Math.round(id*50 + 50).toString();
   }
 }
