c1dffeb85a8b0300acc214543da24d446b3e132c
Sven Moon <svenpire@gmail.com>
2021-03-15 19:43:08 -0400

    order review displays, contactForm started

--- src/app/components/order-review/order-review.component.ts
+++ src/app/components/order-review/order-review.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { SaleOrder } from 'src/app/models/SaleOrder';
+import { SaleOrderDetail } from 'src/app/models/SaleOrderDetail';
+import { selectOrderDetail, selectSaleOrder } from 'src/app/stores/sale-calculator/sale-calculator.selectors';
 
 @Component({
   selector: 'app-order-review',
@@ -6,11 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-review.component.scss']
 })
 export class OrderReviewComponent implements OnInit {
+  orderDetails$: Observable<SaleOrderDetail[]>
+  saleOrder$: Observable<SaleOrder>
+  total: number
+  contactForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private _store: Store<any>,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.orderDetails$ = this._store.pipe(select(selectOrderDetail))
+    this.saleOrder$ = this._store.pipe(select(selectSaleOrder))
+    this.saleOrder$.subscribe(so => this.total = so.total)
 
+    this.contactForm = this.fb.group({
+      name: [null],
+      lastName: [null],
+      email: [null],
+      phone: [null]
+    })
   }
 
 }
