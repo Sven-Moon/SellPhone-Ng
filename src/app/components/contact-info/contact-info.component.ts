ae57677f62455835bdff702beb43f78245ef32de
Sven Moon <svenpire@gmail.com>
2021-03-17 13:21:12 -0400

    contact-info under order-review + ngrx-forms start

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-info',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-info.component.scss']
 })
 export class ContactInfoComponent implements OnInit {
+  contactForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    // instantiate form
+    this.contactForm = this.fb.group({
+      name: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [null, Validators.required],
+      phone: [null, Validators.required]
+    })
   }
-
 }
